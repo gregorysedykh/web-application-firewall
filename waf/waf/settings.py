8d1feb1d2b0a73d1c95cf03f2ebadd2a0dd7dcf1
Gregory Sedykh <62842044+gregorysedykh@users.noreply.github.com>
2025-05-02 18:49:00 +0200

    fix

--- waf/waf/settings.py
+++ waf/waf/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#sz)bvt+*c61)65grxyygy__o#hbgas9hjrd2a743w7sqnj#xc"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
