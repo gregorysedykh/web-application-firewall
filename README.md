5dbec93d7571c82dd4d4504e650fed34c51297f6
Gregory Sedykh <62842044+gregorysedykh@users.noreply.github.com>
2025-05-09 18:26:38 +0200

    ~ increased paranoia to 3 + added instructions to readme + added basic patterns to django firewall + logging + added sanitisation

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # web-application-firewall
+**Advanced Security (14X040) project**
+
+_Gregory Sedykh_
+
+# Description
+This project for the Advanced Security course demonstrates how to set up a web application firewall (WAF) using nginx, ModSecurity and the OWASP Core Rule Set (CRS) that will protect a simple Django application with an input form.
+
+Both the WAF and the Django application are running in Docker containers.
+
+
+# Installation
+
+Add an `.env` file to the root of the project with the following variable:
+```
+DJANGO_SECRET_KEY=your_secret_key
+```
+
+To start the project, run the following command:
+```
+docker compose up
+```
+
+To test the project, go to: 
+```
+http://localhost/test/
+```
--- waf/firewall/middleware.py
+++ waf/firewall/middleware.py
@@ -1,4 +1,5 @@
 import re
+import logging
 from django.http import HttpResponseForbidden
 
 class WAFMiddleware:
@@ -7,10 +8,31 @@ class WAFMiddleware:
         self.patterns = [
             re.compile(r"(select).+from", re.IGNORECASE),
             re.compile(r"<script.*?>", re.IGNORECASE),
+            re.compile(r"(\s|;)+(union)(\s|;)+.*select", re.IGNORECASE),
+            re.compile(r"--['\"].*$", re.IGNORECASE),
+            re.compile(r"on\w+\s*=", re.IGNORECASE),
+            re.compile(r"javascript:", re.IGNORECASE),
+            re.compile(r"\.\./(\.\.)+", re.IGNORECASE),
+            re.compile(r";\s*(cat|ls|pwd|whoami|rm|echo)", re.IGNORECASE),
         ]
+
+        self.logger = logging.getLogger('waf.security')
 
     def __call__(self, request):
         data = " ".join(request.GET.values()) + " " + " ".join(request.POST.values())
-        if any(p.search(data) for p in self.patterns):
-            return HttpResponseForbidden("Forbidden: Malicious input detected.")
+        
+        path = request.path
+        full_data = data + " " + path
+        
+        for pattern in self.patterns:
+            match = pattern.search(full_data)
+            if match:
+
+                self.logger.warning(
+                    f"Blocked request from IP={request.META.get('REMOTE_ADDR')}, "
+                    f"Pattern={pattern.pattern}, "
+                    f"Data={full_data[:100] + '...' if len(full_data) > 100 else full_data}"
+                )
+                return HttpResponseForbidden("403 Forbidden")
+                
         return self.get_response(request)
